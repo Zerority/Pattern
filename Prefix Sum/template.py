58ac5b15b9b381e6a9e4a87601c8bd7419bc15e2
Nguyễn Quốc Bảo <144548281+Zerority@users.noreply.github.com>
2026-07-28 14:29:18 +0700

    Create template.py

---
+++ Prefix Sum/template.py
@@ -0,0 +1,21 @@
+nums = [1,4,6,8]
+Prefix_sum = [0]
+for i in range(len(nums)):
+    num = Prefix_sum[-1] + nums[i]
+    Prefix_sum.append(num)
+print(Prefix_sum)
+
+#Commencer avec le numéro 0 dans le "prefix sum", nums[i] => nums[0] => 1
+#num = Prefix_sum[-1] => La dernière chiffre dans l'array, => num = 0 + 1 = 1 => Prefix_sum = [0,1]
+#Après, Prefix_sum[-1] = 1, parce que 1 est la dernière chiffre maintenant, => num = 1 + 4 = 5
+#Après cette boucle, => Prefix_sum = [0, 1, 5, 11, 19]
+#Pour calculer la somme de cet array, par example, l'index 0 et l'index 3
+#=>
+answer = Prefix_sum[4] - Prefix_sum[0]
+#Plus généralement :
+answer = Prefix_sum[right+1] - Prefix_sum[left]
+#Notes:
+#Ici, "left" et "right" représentent la distance. 
+#Pourquoi right + 1 ? 
+#Parce que vous devez calculer la somme de l'index 1 et l'index 3 dans (nums)
+#=> l'index 3 dans (nums) est l'index 3 + 1 = 4 dans Prefix Sum
